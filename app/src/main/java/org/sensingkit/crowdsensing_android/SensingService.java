a73f568fe6b7a97c52052966751b0252001357de
Minos Katevas <minos.kat@gmail.com>
2015-02-19 19:15:49 +0000

    Add WakeLock

--- app/src/main/java/org/sensingkit/crowdsensing_android/SensingService.java
+++ app/src/main/java/org/sensingkit/crowdsensing_android/SensingService.java
@@ -22,12 +22,15 @@
 package org.sensingkit.crowdsensing_android;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.util.Log;
 
 import org.sensingkit.sensingkitlib.SKException;
+import org.sensingkit.sensingkitlib.SKUtilities;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -39,6 +42,8 @@ public class SensingService extends Service {
     protected static final String TAG = "SensingService";
 
     private final IBinder mBinder = new LocalBinder();
+
+    private PowerManager.WakeLock mWakeLock;
 
     // Sensing Session
     private SensingSession mSensingSession;
@@ -73,6 +78,14 @@ public class SensingService extends Service {
         super.onDestroy();
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        // We want this service to continue running until it is explicitly
+        // stopped, so return sticky.
+        return START_STICKY;
+    }
+
     /**
      * Class used for the client Binder.  Because we know this service always
      * runs in the same process as its clients, we don't need to deal with IPC.
@@ -103,13 +116,29 @@ public class SensingService extends Service {
         return session;
     }
 
+    // --- Wake Lock
+
+    private void acquireWakeLock() {
+        if ((mWakeLock == null) || (!mWakeLock.isHeld())) {
+            PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WakeLock");
+            mWakeLock.acquire();
+        }
+    }
+
+    private void releaseWakeLock() {
+        if (mWakeLock != null && mWakeLock.isHeld()) {
+            mWakeLock.release();
+        }
+    }
 
     // --- Public API
 
     public void startSensing() {
 
        try {
-            mSensingSession.start();
+           acquireWakeLock();
+           mSensingSession.start();
         }
         catch (SKException ex) {
             ex.printStackTrace();
@@ -119,6 +148,7 @@ public class SensingService extends Service {
     public void stopSensing() {
 
         try {
+            releaseWakeLock();
             mSensingSession.stop();
         }
         catch (SKException ex) {
@@ -129,6 +159,7 @@ public class SensingService extends Service {
     public void startCalibration() {
 
         try {
+            acquireWakeLock();
             mSensingSession.calibrateStart();
         }
         catch (SKException ex) {
@@ -139,6 +170,7 @@ public class SensingService extends Service {
     public void stopCalibration() {
 
         try {
+            releaseWakeLock();
             mSensingSession.calibrateStop();
         }
         catch (SKException ex) {
