d75b89757eda6d308dba6f2a67e7d876f24402da
Kleomenis Katevas <minos.kat@gmail.com>
2020-07-27 14:49:29 +0200

    Remove unused menu

--- app/src/main/java/org/sensingkit/crowdsensing_android/CrowdSensing.java
+++ app/src/main/java/org/sensingkit/crowdsensing_android/CrowdSensing.java
@@ -134,13 +134,6 @@ public class CrowdSensing extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.crowd_sensing, menu);
-        return true;
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
 
